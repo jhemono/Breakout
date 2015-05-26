9c9642c3935d001b0c9c4a8506b01badf2670af5
Julien Hémono <jhemono@me.com>
2015-05-26 16:01:03 +0200

    Bouncing ball on tap

---
+++ Breakout/BreakoutBehavior.swift
@@ -0,0 +1,73 @@
+//
+//  BreakoutBehavior.swift
+//  Breakout
+//
+//  Created by Julien Hémono on 26/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+protocol BoundaryItem {
+    var key: String { get }
+    var frame: CGRect { get }
+}
+
+class BreakoutBehavior: UIDynamicBehavior {
+    lazy var collider: UICollisionBehavior = {
+        let collider = UICollisionBehavior()
+        collider.translatesReferenceBoundsIntoBoundary = true
+        return collider
+    }()
+    
+    lazy var ballBehavior: UIDynamicItemBehavior = {
+        let ballBehavior = UIDynamicItemBehavior()
+        ballBehavior.elasticity = 1.0
+        ballBehavior.allowsRotation = true
+        return ballBehavior
+    }()
+    
+    override init() {
+        super.init()
+        addChildBehavior(collider)
+        addChildBehavior(ballBehavior)
+    }
+    
+    func addBrick(brick: BoundaryItem) {
+        let path = UIBezierPath(rect: brick.frame)
+        collider.addBoundaryWithIdentifier(brick.key, forPath: path)
+    }
+    
+    func updatePaddle(paddle: BoundaryItem) {
+        collider.removeBoundaryWithIdentifier(paddle.key)
+        let path = UIBezierPath(ovalInRect: paddle.frame)
+        collider.addBoundaryWithIdentifier(paddle.key, forPath: path)
+    }
+    
+    func removeBoundaryItem(boundaryItem: BoundaryItem) {
+        collider.removeBoundaryWithIdentifier(boundaryItem.key)
+    }
+    
+    func addBall(ball: UIView) {
+        dynamicAnimator?.referenceView?.addSubview(ball)
+        ballBehavior.addItem(ball)
+        collider.addItem(ball)
+    }
+    
+    func pushBall(ball: UIView) {
+        if let pushBehavior = UIPushBehavior(items: [ball], mode: .Instantaneous) {
+            pushBehavior.magnitude = CGFloat.randomRatio
+            pushBehavior.angle = CGFloat(2 * M_PI) * CGFloat.randomRatio
+            pushBehavior.action = { [unowned pushBehavior] in
+                pushBehavior.dynamicAnimator?.removeBehavior(pushBehavior)
+            }
+            dynamicAnimator?.addBehavior(pushBehavior)
+        }
+    }
+}
+
+extension CGFloat {
+    static var randomRatio: CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
--- Breakout/GameViewController.swift
+++ Breakout/GameViewController.swift
@@ -8,7 +8,45 @@
 
 import UIKit
 
+struct Paddle: BoundaryItem {
+    let key = "Paddle"
+    var view: UIView
+    var frame: CGRect { return view.frame }
+}
+
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var gameView: GameView!
+    
+    lazy var animator: UIDynamicAnimator = { UIDynamicAnimator(referenceView: self.gameView!) }()
+    
+    let breakoutBehavior = BreakoutBehavior()
+    
+    lazy var paddle: UIView = {
+        let gameView = self.gameView!
+        let paddle = UIView(frame: CGRect(x: gameView.frame.midX, y: gameView.frame.maxY - 30, width: 30, height: 10))
+        paddle.backgroundColor = UIColor.orangeColor()
+        gameView.addSubview(paddle)
+        return paddle
+    }()
+    
+    lazy var ball: UIView = {
+        let paddle = self.paddle
+        let ball = UIView(frame: CGRect(x: paddle.frame.midX, y: paddle.frame.minY-20.0, width: 20.0, height: 20.0))
+        ball.backgroundColor = UIColor.purpleColor()
+        return ball
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        animator.addBehavior(breakoutBehavior)
+        
+        breakoutBehavior.updatePaddle(Paddle(view: paddle))
+        breakoutBehavior.addBall(ball)
+    }
+    @IBAction func pushBall(sender: UITapGestureRecognizer) {
+        breakoutBehavior.pushBall(ball)
+    }
 }
 
