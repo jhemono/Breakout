956a9b9fc558a60a165912367ecbe55bfd98eae5
Julien Hémono <jhemono@me.com>
2015-05-26 13:09:39 +0200

    Setting up

---
+++ Breakout/GameView.swift
@@ -0,0 +1,21 @@
+//
+//  GameView.swift
+//  Breakout
+//
+//  Created by Julien Hémono on 26/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+class GameView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Breakout/GameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GameViewController.swift
+//  Breakout
+//
+//  Created by Julien Hémono on 26/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+}
+
